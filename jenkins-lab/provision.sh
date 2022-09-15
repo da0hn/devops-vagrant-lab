40840f09712e8544d827fe3e0d91cf2329e41bd3
Da0hn <gabrielhondacba@gmail.com>
2022-09-14 22:18:16 -0400

    Add config of vagrant and provision to jenkins lab

---
+++ jenkins-lab/provision.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+yum install epel-release -y
+yum install wget -y
+yum install java-11-openjdk-devel -y
+
+echo "Installing docker..."
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Installing docker compose..."
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+echo "Installing Jenkins and dependencies..."
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl restart docker
